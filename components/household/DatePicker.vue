404cb7c775e01e9a3014e2059b395005e9ed79c3
ryo yoneyama <uryonet@gmail.com>
2019-12-08 14:09:28 +0900

    カレンダーコンポーネントの実装

--- components/household/DatePicker.vue
+++ components/household/DatePicker.vue
@@ -1,11 +1,34 @@
 <template>
   <v-flex>
     <v-date-picker
+      v-model="picker"
       full-width
       type="month"
       no-title
       locale="ja"
       class="v-card--outlined"
+      show-current="false"
     ></v-date-picker>
   </v-flex>
 </template>
+
+<script>
+export default {
+  computed: {
+    picker: {
+      get() {
+        return this.$store.getters.getCurrentMonth
+      },
+      set(value) {
+        this.$store.commit('setCurrentMonth', value)
+      }
+    }
+  },
+  created() {
+    const today = new Date()
+    const currentMonth =
+      today.getFullYear() + '-' + ('00' + (today.getMonth() + 1)).slice(-2)
+    this.$store.commit('setCurrentMonth', currentMonth)
+  }
+}
+</script>
--- store/index.js
+++ store/index.js
@@ -1,18 +1,24 @@
 import cloneDeep from 'lodash.clonedeep'
 
 export const state = () => ({
-  user: null
+  user: null,
+  currentMonth: null,
 })
 
 export const getters = {
   isAuthenticated(state) {
     return state.user ? state.user.email === 'uryonet@gmail.com' : false
   }
+  },
+  getCurrentMonth: (state) => state.currentMonth,
 }
 
 export const mutations = {
   setUser(state, { user }) {
     state.user = cloneDeep(user)
+  },
+  setCurrentMonth(state, payload) {
+    state.currentMonth = payload
   }
 }
 
