ecb6b2044cf7c05f52f79c67b35f0475d909ae00
ryo yoneyama <uryonet@gmail.com>
2019-12-31 07:30:09 +0900

    収支明細をデータから取得して表示するように修正

--- components/household/IncomeList.vue
+++ components/household/IncomeList.vue
@@ -2,12 +2,14 @@
   <v-flex xs12>
     <v-card outlined>
       <v-subheader>収入詳細</v-subheader>
+      <v-divider></v-divider>
       <v-simple-table>
         <template v-slot:default>
           <tbody>
             <tr v-for="(item, key) in findIncomeList" :key="key">
-              <td>{{ item.title }}</td>
-              <td>{{ item.amount }}</td>
+              <td>{{ dispDate(item.date) }}</td>
+              <td class="text-center">{{ item.title }}</td>
+              <td class="text-right">{{ separate(item.amount) }}</td>
             </tr>
           </tbody>
         </template>
@@ -21,6 +23,14 @@ export default {
   computed: {
     findIncomeList() {
       return this.$store.getters.getIncomeList
+    }
+  },
+  methods: {
+    dispDate(date) {
+      return ('0' + date.toDate().getDate()).slice(-2) + '日'
+    },
+    separate(num) {
+      return '¥ ' + String(num).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,')
     }
   }
 }
